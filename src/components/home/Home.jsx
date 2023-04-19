1104f0456c654a84474571d548f94c7ca6f0c7ee
Csb-218 <96022278+Csb-218@users.noreply.github.com>
2023-04-20 02:14:45 +0530

    updated layout

--- src/components/home/Home.jsx
+++ src/components/home/Home.jsx
@@ -10,7 +10,7 @@ function Home({data}) {
 
             <Link key={ev.id} href={`/events/${ev.id}`} className={ data.indexOf(ev) % 2===0 ?'flex lg:flex-row-reverse flex-col lg:my-10 my-2 justify-center ':'flex lg:flex-row flex-col lg:my-10 my-2 justify-center '}>
                 <Image width={400} height={400} alt={ev.title} src={ev.image} className='w-auto h-auto'/>
-                <div className='my-32 mx-10'>
+                <div className='my-20 mx-10'>
                   <h2 className="text-4xl mt-4">{ev.title}</h2>
                   <p>{ev.description}</p>
                 </div>
